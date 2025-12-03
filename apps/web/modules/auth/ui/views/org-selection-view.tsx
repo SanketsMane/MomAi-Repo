5dc89301630c234bf209d2b890c2d5cdc772a3a8
SanketsMane <sanketpatil515151@gmail.com>
2025-12-03 19:38:08 +0530

    Remove unsupported fallbackRedirectUrl prop from OrganizationList

--- apps/web/modules/auth/ui/views/org-selection-view.tsx
+++ apps/web/modules/auth/ui/views/org-selection-view.tsx
@@ -3,7 +3,6 @@ import { OrganizationList } from "@clerk/nextjs";
 export const OrgSelectionView = () => {
   return (
     <OrganizationList
-      fallbackRedirectUrl="/dashboard"
       hidePersonal
       skipInvitationScreen
     />
